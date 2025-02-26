68b1dd7de7f3f750f01804dad242fb6d6cdbedcb
tasneemkhaled <khaledtasneem402@gmail.com>
2025-02-26 23:12:52 +0200

    final copy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/services/news_service.dart';
+
 import 'package:news_app/views/home_view.dart';
 
 void main() {
